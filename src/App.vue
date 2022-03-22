ca84c841e49f1f0ad553479936b5fadff3f7fcb9
ecabigting <ericcabigting@outlook.com>
2022-03-22 10:05:15 +0400

    - fix annoying eslint
    - add code to headernav
    
    - show it in main app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,18 @@
 <template>
+  <HeaderNav/>
   <router-view />
 </template>
+
+
+<script>
+import HeaderNav from './components/HeaderNav.vue'
+
+export default {
+  components: {
+    HeaderNav
+  }
+}
+</script>
 
 <style>
 
--- src/components/HeaderNav.vue
+++ src/components/HeaderNav.vue
@@ -1,3 +1,32 @@
 <template>
-  <header class="flex w-full items-center justify-center p-4"></header>
+  <header class="flex w-full items-center justify-center p-4">
+    <div
+      :class="`menu-toggle relative z-50
+       ${menu_is_active ? 'is_active' : ''}`"
+      @click="ToggleMenu"
+    ></div>
+
+    <div class="hamburger">
+      <span></span>
+    </div>
+  </header>
 </template>
+
+<script>
+import { computed } from "vue";
+import { useStore } from "vuex";
+
+export default {
+  setup() {
+    const store = useStore();
+    const ToggleMenu = () => {
+      store.dispatch('ToggleMenu')
+    }
+
+    return {
+      menu_is_active: computed(() => store.state.menu_is_active),
+      ToggleMenu
+    };
+  },
+};
+</script>
