67c8527056e8d1d14a0e8925acf6d0b0845087fd
ecabigting <ericcabigting@outlook.com>
2022-03-25 10:25:33 +0400

    added new component update the main home view

---
+++ src/components/PostCard.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="bg-gray-800 rounded-lg p-4 flex flex-col sm:flex-row">
+        
+    </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="home">
-    <div>Home</div>
-  </div>
+  <main class="home">
+    <section class="container mx-auto p-4">
+      <h1 class="text-2xl mb-8">Latest Post</h1>
+      <div class="grid gap-4">
+        
+      </div>
+    </section>
+  </main>
 </template>
