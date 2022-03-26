97bbf4f6a044ed7674fea545c3996e16da5f3a85
ecabigting <ericcabigting@outlook.com>
2022-03-26 13:28:34 +0400

    started building posts list and GROQs

--- src/components/PostCard.vue
+++ src/components/PostCard.vue
@@ -1,5 +1,36 @@
 <template>
     <div class="bg-gray-800 rounded-lg p-4 flex flex-col sm:flex-row">
-        
+        <img v-if="post.image" src="" alt=""/>
+        <div class="flex-1 flex flex-col">
+            <h3 class="text-lg md:text-2xl font-bold mb-4">{{post.title}}</h3>
+            <p class="text-gray-500 md:text-lg mb-4 flex-1">{{post.exceprt}}</p>
+            <div class="flex sm:flex-col md:flex-row justify-between items-start md:items-end">
+
+                <div class="flex items-0center sm:mb-4 md:mb-0">
+                    <img v-if="post.author.avatar" src="" alt=""/>
+                    <div class="">
+                        <p class="text-white mr-4">
+                            {{post.author.full_name}}
+                        </p>
+                        <p class="text-gray-500 text-sm">
+                            {{post._created_at}}
+                        </p>
+                    </div>
+                </div>
+
+                <router-link :to="`/post/${post._id}`" class="btn">read more</router-link>
+            </div>
+        </div>
     </div>
 </template>
+
+<script>
+export default {
+    props: {
+         post: {
+             type: Object,
+             required : true
+         }
+     }  
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,9 +2,31 @@
   <main class="home">
     <section class="container mx-auto p-4">
       <h1 class="text-2xl mb-8">Latest Post</h1>
-      <div class="grid gap-4">
-        
-      </div>
+      <div class="grid gap-4"></div>
     </section>
   </main>
 </template>
+
+<script>
+import { onMounted, onMounted, ref, computed } from "vue";
+import { useStore } from "vuex";
+import sanity from "../client";
+import PostCard from "../components/PostCard.vue";
+
+export default {
+  components: {
+    PostCard,
+  },
+  setup() {
+    const subscription = ref(null);
+    const store = userStore();
+
+    const posts = computed(() => store.getters.posts);
+
+    onMounted(()=>{
+      store.dispatch("FetchPosts",3)
+
+    })
+  },
+};
+</script>
