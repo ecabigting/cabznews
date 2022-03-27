dd884da13e824de67453a6478d6d694081b6d62c
ecabigting <ericcabigting@outlook.com>
2022-03-27 12:37:34 +0400

    added code to start using groq

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,13 +2,15 @@
   <main class="home">
     <section class="container mx-auto p-4">
       <h1 class="text-2xl mb-8">Latest Post</h1>
-      <div class="grid gap-4"></div>
+      <div class="grid gap-4">
+        <PostCard v-for="(posts,i) in posts" :key="i" :post="post"/>
+      </div>
     </section>
   </main>
 </template>
 
 <script>
-import { onMounted, onMounted, ref, computed } from "vue";
+import { onMounted, onUnmounted, ref, computed } from "vue";
 import { useStore } from "vuex";
 import sanity from "../client";
 import PostCard from "../components/PostCard.vue";
@@ -19,14 +21,35 @@ export default {
   },
   setup() {
     const subscription = ref(null);
-    const store = userStore();
+    const store = useStore();
 
     const posts = computed(() => store.getters.posts);
 
     onMounted(()=>{
       store.dispatch("FetchPosts",3)
-
+      const query = '*[_type === "post"]'
+      subscription.value = sanity
+        .listen(query)
+        .subscribe(update => {
+            switch(update.transition) {
+              case 'update' : 
+                console.log("Post Updated!",update)
+                break;
+              case 'appear':
+                console.log("Post Appeared!",update)
+                break;
+              case 'disappear':
+                console.log("Post Disappeared!",update)
+                break;
+            }
+        })
     })
+
+    onUnmounted(()=>{
+      subscription.value.unsubscribe()
+    });
+
+    return { posts }
   },
 };
 </script>
