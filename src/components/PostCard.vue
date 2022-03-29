6bb4551e346ec55a4bfead3fc40321fe6bb92c2e
ecabigting <ericcabigting@outlook.com>
2022-03-29 12:44:56 +0400

    - added sanity/image-url package
     - added new file utils.js to handle date formatting and image source url
    
     - update postcard to show proper date and image

--- src/components/PostCard.vue
+++ src/components/PostCard.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="bg-gray-800 rounded-lg p-4 flex flex-col sm:flex-row">
-        <img v-if="post.image" src="" alt=""/>
+        <img v-if="post.image" :src="CreateURL(post.image,480, 320)" alt="block w-full sm:max-w-xs mr-4 object-cover mb-4 sm:mb-0"/>
         <div class="flex-1 flex flex-col">
             <h3 class="text-lg md:text-2xl font-bold mb-4">{{post.title}}</h3>
             <p class="text-gray-500 md:text-lg mb-4 flex-1">{{post.exceprt}}</p>
@@ -13,7 +13,7 @@
                             {{post.author.full_name}}
                         </p>
                         <p class="text-gray-500 text-sm">
-                            {{post._created_at}}
+                            {{ FormatDate(post._createdAt) }}
                         </p>
                     </div>
                 </div>
@@ -25,12 +25,19 @@
 </template>
 
 <script>
+import { FormatDate, CreateURL} from '../utils'
 export default {
     props: {
          post: {
              type: Object,
              required : true
          }
-     }  
+     },  
+    setup(){
+        return {
+            FormatDate,
+            CreateURL
+        }
+    }
 }
 </script>
