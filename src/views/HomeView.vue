09154b2476891b7722244fad78117d0c44b3b02d
ecabigting <ericcabigting@outlook.com>
2022-03-31 20:45:06 +0400

    fix some code formatting

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,7 @@
     <section class="container mx-auto p-4">
       <h1 class="text-2xl mb-8">Latest Post</h1>
       <div class="grid gap-4">
-        <PostCard v-for="(posts,i) in posts" :key="i" :post="post"/>
+        <PostCard v-for="(posts, i) in posts" :key="i" :post="post" />
       </div>
     </section>
   </main>
@@ -25,41 +25,40 @@ export default {
 
     const posts = computed(() => store.getters.posts);
 
-    onMounted(()=>{
-      store.dispatch("FetchPosts",3)
-      const query = '*[_type == "post"]'
-      subscription.value = sanity
-        .listen(query)
-        .subscribe(update => {
-            switch(update.transition) {
-              case 'update' : 
+    onMounted(() => {
+      store.dispatch("FetchPosts", 3);
+      const query = '*[_type == "post"]';
+      subscription.value = sanity.listen(query).subscribe((update) => {
+        switch (update.transition) {
+          case "update":
+            sanity.getDocument(update.result.author._ref).then((author) => {
+              store.dispatch("UpdatePost", {
+                ...update.result,
+                author,
+              });
+            });
 
-                sanity.getDocument(update.result.author._ref).then(author => {
-                  store.dispatch('UpdatePost',{
-                    ...update.result, author
-                  })
-                })
-                
-                break;
-              case 'appear':
-                sanity.getDocument(update.result.author._ref).then(author => {
-                  store.dispatch('AddNewPost',{
-                    ...update.result, author
-                  })
-                })
-                break;
-              case 'disappear':
-                console.log("Post Disappeared!",update)
-                break;
-            }
-        })
-    })
-
-    onUnmounted(()=>{
-      subscription.value.unsubscribe()
+            break;
+          case "appear":
+            sanity.getDocument(update.result.author._ref).then((author) => {
+              store.dispatch("AddNewPost", {
+                ...update.result,
+                author,
+              });
+            });
+            break;
+          case "disappear":
+            console.log("Post Disappeared!", update);
+            break;
+        }
+      });
     });
 
-    return { posts }
+    onUnmounted(() => {
+      subscription.value.unsubscribe();
+    });
+
+    return { posts };
   },
 };
 </script>
