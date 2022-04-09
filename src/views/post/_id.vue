4d574e708a57cd6082f6d21c19a8f176008d7ef6
ecabigting <ericcabigting@outlook.com>
2022-04-09 19:37:15 +0400

    added single post page

---
+++ src/views/post/_id.vue
@@ -0,0 +1,12 @@
+<template>
+    <main class="post-page">
+        <!-- Image -->
+        <img src="" alt="">
+        <!-- Back button -->
+        <button>Back</button>
+        <!-- title -->
+        <!-- Excerpt -->
+        <!-- content -->
+        <!-- author details -->
+    </main>
+</template>
