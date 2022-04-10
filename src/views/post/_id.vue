1ca157fd375b087fec3a165180263506b10001a0
ecabigting <ericcabigting@outlook.com>
2022-04-10 12:50:01 +0400

    - update routes to add single post page
    - added script to do post fetch on specific post id

--- src/views/post/_id.vue
+++ src/views/post/_id.vue
@@ -10,3 +10,33 @@
         <!-- author details -->
     </main>
 </template>
+
+<script>
+import { onMounted, ref } from 'vue'
+import { useRoute } from 'vue-router'
+import sanity from '../../client'
+import { CreateURL } from '../../utils'
+
+
+export default {
+    setup(){
+        const route = useRoute()
+        const id = ref(route.params.id);
+        const post = ref(null);
+
+        onMounted(()=>{
+            const query = `*[_type == "post" && _id == $id][0] {...,author => }`
+            const params = { id : id.value }
+
+            sanity.fetch(query,params).then(data => {
+                post.value = data;
+            })
+        })
+
+        return {
+            post,
+            CreateURL
+        }
+    }
+}
+</script>
